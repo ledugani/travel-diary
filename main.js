26fcc157f9c5f9bb94de95d966cfdb26975765b6
Tom Dugan <thomas@aadwebb.com>
2018-03-22 19:42:15 -0500

    created domstring function

--- main.js
+++ main.js
@@ -1,32 +1,47 @@
+
 var destinationCards = [
     {
         title: "St. Thomas, USVI",
         image: "http://usvitoday.com/wp-content/uploads/2017/05/USVI-Bluebeards_beach_club-750x500.jpg",
-        description: "Type your entry here"
+        description: "stthomas"
     },
     {
         title: "Seattle, WA",
         image: "https://upload.wikimedia.org/wikipedia/commons/2/2f/Space_Needle002.jpg",
-        description: "Type your entry here"
+        description: "seattle"
     },
     {
         title: "Las Vegas, NV",
         image: "https://www.sportsgamblingpodcast.com/wp-content/uploads/2018/01/guide-to-las-vegas-2018-1.jpg",
-        description: "Type your entry here"
+        description: "lasvegas"
     },
     {
         title: "Key West, FL",
         image: "https://img.marinas.com/v2/c1cc413c737ba7ba9568094d6a3b3da45933ac2bdd846d69b7c73fac0b0a66ed.jpg",
-        description: "Type your entry here"
+        description: "keywest"
     },
     {
         title: "Phoenix, AZ",
         image: "https://cdn.moneycrashers.com/wp-content/uploads/2017/07/full-moon-phoenix-arizona-skyline-918x516.jpg",
-        description: "Type your entry here"
+        description: "phoenix"
     }
 ];
 
 const printToDom = (domString, divId) => {
-    const printTo = document.getElementById(divId);
-    printTo.innerHTML = domString;
+    document.getElementById(divId).innerHTML = domString;
 };
+
+const buildDomString = (destination) => {
+    let domString = '';
+    destination.forEach((card) => {
+        domString += `<div class="card">`;
+        domString += `<h1>${card.title}</h1>`;
+        domString += `<img src="${card.image}" class="picture">`;
+        domString += `<input type='text' name='${card.description}' value='Type your entry here...'>`;
+        domString += `<button class="card-button">Submit</button>`;
+        domString += `</div>`;
+    });
+    printToDom(domString, 'card-holder');
+}
+
+buildDomString(destinationCards);
